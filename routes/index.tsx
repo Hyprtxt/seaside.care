c3ce14ab54273f04f547803a6ebf4e8664a88555
Taylor Y <tdy721@gmail.com>
2023-05-16 19:16:59 -0700

    ok

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,12 +4,20 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Seaside Pharmacy</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <h2>Seaside Pharmacy</h2>
-        <a>781-284-6525</a>
+      <div class="p-4 mx-auto text-center max-w-xs my-5">
+        <h2
+          class="font-lora uppercase relative mb-2"
+          style="font-size: 4.15rem; line-height: 0.7;"
+        >
+          Seaside<span class="text-5xl inline-block">Pharmacy</span>
+        </h2>
+        <a href="tel:781-284-6525">781-284-6525</a>
         <p>Revere, MA</p>
+        <a href="https://goo.gl/maps/ZuBSQdKqEavcKrgf9?coh=178571&entry=tt">
+          Find us on Google Maps
+        </a>
       </div>
     </>
   );
--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,36 @@
 import { Options } from "$fresh/plugins/twind.ts";
+import * as colors from "twind/colors"
 
 export default {
   selfURL: import.meta.url,
+  preflight: {
+    "@import":
+      "https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap",
+    "@font-face": [
+      {
+        fontFamily: "Lora",
+      },
+    ],
+  },
+  theme: {
+    extend: {
+      transitionDuration: {
+        "0": "0ms",
+        "1600": "1600ms",
+        "2000": "2000ms",
+      },
+    },
+    fontFamily: {
+      "lora": ['"Lora"', "serif"],
+    },
+    colors: {
+      blue: "rgb(0,153,212)",
+      violet: "rgb(108, 73, 136)",
+      grey: colors.coolGray,
+      dark: "rgb(15,15,15)",
+      transparent: "rgba(0,0,0,0)",
+      white: colors.white,
+      black: colors.black,
+    },
+  },
 } as Options;
